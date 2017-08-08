c72a2497b3121e145d309a2eb8dad608a1c8a55f
Armicula <armicula@kiafaldorius.net>
2017-08-07 22:25:39 -0700

    early prototype...

---
+++ src/main.c
@@ -0,0 +1,204 @@
+#include <Arduino.h>
+#include <avr/power.h>
+#include <EEPROM.h>
+
+// Pins
+#define MOTOR_A A2
+#define MOTOR_B A4
+#define POWER_CHECK A6
+
+// motor power Levels
+const uint8_t MAX = 255; // 1023/1023
+const uint8_t STRONG = 140; // 90-100mA 560/1023
+const uint8_t WEAK = 120; // 75-90mA 480/1023
+const uint8_t OFF = 0;
+const int MOTOR_START_WAIT = 1000;
+const unsigned long CYCLE_LENGTH = 60000; // 60*1000 = 1 minute
+
+// Voltage Levels
+const int OVERCHARGE = 837; // 3.6v
+const int SOLAR = 814; // 3.5v
+const int FULL = 791; // 3.4v
+const int CHARGED = 767; // 3.3v
+const int NOMINAL = 744; // 3.2v
+const int DRAINED = 721; // 3.1v
+const int CUTOFF = 697; // 3.0v
+const int TOLERANCE = 50; // 0.05v
+
+// globals
+bool motorA = false;
+bool motorB = false;
+uint8_t cycle = 0;
+const bool A = true;
+const bool B = false;
+
+// we have 1024 bytes of EEPROM and will be using 512 bytes for log storage.
+// marker at position 0 tells us where we are.
+unsigned int logPos = 3; // first two bytes is reserved for logPointer
+const unsigned int logStart = 3;
+
+void clearLogs(){
+  for (unsigned int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+}
+
+void loadLogPosition(){
+  // You should probably clearLogs() before using it the first time.
+  uint8_t high_byte = EEPROM.read(0);
+  uint8_t low_byte = EEPROM.read(1);
+  logPos = high_byte * 256 + low_byte;
+  if(logPos < logStart){ logPos = logStart; }
+}
+
+void setup(){
+  pinMode(MOTOR_A, OUTPUT);
+  pinMode(MOTOR_B, OUTPUT);
+  pinMode(POWER_CHECK, INPUT);
+  analogReference(INTERNAL);
+  // First few readings after changing analogReference will be wrong.
+  analogRead(POWER_CHECK);
+  delay(500);
+  analogRead(POWER_CHECK);
+  delay(300);
+  analogRead(POWER_CHECK);
+  delay(200);
+  analogRead(POWER_CHECK);
+  // end analogReference setup
+  power_twi_disable();
+  power_spi_disable();
+  loadLogPosition();
+}
+
+void runMotor(bool useMotorA, uint8_t power){
+  if(useMotorA && motorA){
+    // motor A - running - change the power level
+    analogWrite(MOTOR_A, power);
+  } else if(!useMotorA && motorB){
+    // motor B - running - change the power Level
+    analogWrite(MOTOR_B, power);
+  } else if(useMotorA && !motorA){
+    analogWrite(MOTOR_A, MAX);
+    delay(MOTOR_START_WAIT);
+    analogWrite(MOTOR_A, power);
+    motorA = true;
+  } else if(!useMotorA && !motorB){
+    analogWrite(MOTOR_B, MAX);
+    delay(MOTOR_START_WAIT);
+    analogWrite(MOTOR_B, power);
+    motorB = true;
+  }
+}
+
+void stopMotor(bool useMotorA){
+  if(useMotorA){
+    digitalWrite(MOTOR_A, LOW);
+    motorA = false;
+  } else {
+    digitalWrite(MOTOR_B, LOW);
+    motorB = false;
+  }
+}
+
+void saveCycle(uint8_t power, uint8_t cycle_time){
+  // we save the power on odd byte, then cycle+cycle_time next (even)
+  // NOTE: power has been scaled down from 1023 to 255 to save space, low res
+
+  // this should not happen...we should always be on an odd byte when called
+  if(logPos % 2 == 1){ logPos++; }
+  // we will overflow, cycle back around to the start
+  if((logPos + 2) >= EEPROM.length()){ logPos = logStart; }
+
+  uint8_t cycling = (cycle_time << 2) + cycle;
+  EEPROM.write(logPos++, power);
+  EEPROM.write(logPos++, cycling);
+  EEPROM.write(0, (uint8_t)(logPos >> 8));
+  EEPROM.write(1, (uint8_t)(logPos));
+}
+
+void loop(){
+  uint8_t cycle_time = 1;
+  int power = analogRead(POWER_CHECK);
+
+  if(power > SOLAR + TOLERANCE){
+    // Power is higher than the solar panel, which means battery is over-charged.
+    // In actual use, this should only happen when a new fully charged battery
+    // was put in to replace the old battery. We use up power to bring it back down.
+    runMotor(A, STRONG);
+    runMotor(B, STRONG);
+  } else if(power > FULL + TOLERANCE){
+    if(cycle == 0){
+      runMotor(A, STRONG);
+      stopMotor(B);
+      cycle_time = 2;
+    } else if(cycle == 1){
+      runMotor(A, WEAK);
+      runMotor(B, WEAK);
+      cycle_time = 1;
+    } else if(cycle == 2){
+      stopMotor(A);
+      runMotor(B, STRONG);
+      cycle_time = 2;
+    } else if(cycle == 3){
+      stopMotor(A);
+      stopMotor(B);
+      cycle_time = 5;
+    }
+  } else if(power > CHARGED + TOLERANCE){
+    if(cycle == 0 || cycle == 2){
+      stopMotor(A);
+      stopMotor(B);
+      cycle_time = 9;
+    } else if(cycle == 1){
+      runMotor(A, WEAK);
+      stopMotor(B);
+    } else if(cycle == 3){
+      stopMotor(A);
+      runMotor(B, WEAK);
+    }
+  } else if(power > NOMINAL + TOLERANCE){
+    if(cycle == 0 || cycle == 2){
+      stopMotor(A);
+      stopMotor(B);
+      cycle_time = 29;
+    } else if(cycle == 1){
+      runMotor(A, WEAK);
+      stopMotor(B);
+    } else if(cycle == 3){
+      stopMotor(A);
+      runMotor(B, WEAK);
+    }
+  } else if(power > DRAINED + TOLERANCE){
+    if(cycle == 0 || cycle == 2){
+      stopMotor(A);
+      stopMotor(B);
+      cycle_time = 39;
+    } else if(cycle == 1){
+      runMotor(A, WEAK);
+      stopMotor(B);
+    } else if(cycle == 3){
+      stopMotor(A);
+      runMotor(B, WEAK);
+    }
+  } else if(power > CUTOFF){
+    if(cycle == 0 || cycle == 2){
+      stopMotor(A);
+      stopMotor(B);
+      cycle_time = 59;
+    } else if(cycle == 1){
+      runMotor(A, WEAK);
+      stopMotor(B);
+    } else if(cycle == 3){
+      stopMotor(A);
+      runMotor(B, WEAK);
+    }
+  } else {
+    stopMotor(A);
+    stopMotor(B);
+    cycle_time = 20;
+  }
+  saveCycle(power, cycle_time);
+
+  cycle = (cycle + 1) % 4;
+  delay(CYCLE_LENGTH * cycle_time); // TODO: convert to sleep
+}
