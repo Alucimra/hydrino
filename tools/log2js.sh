7c3a7b5303bc6d86cbed6f3142cb241eafd1759f
Armicula <armicula@kiafaldorius.net>
2017-08-31 23:59:39 -0700

    move logs to its own directory,and add selector to log2js.sh

--- tools/log2js.sh
+++ tools/log2js.sh
@@ -1,4 +1,27 @@
 #!env bash
-echo "window.rawLog = \`" > rawLog.js
-cat current.log >> rawLog.js
-echo "\`;" >> rawLog.js
+shopt -s nullglob
+
+
+TOOLS_DIR=$(realpath $(dirname "$0"));
+LOGS_DIR=$(realpath "$TOOLS_DIR/../logs");
+
+FILES_LIST=($LOGS_DIR/*.log);
+i=0;
+for file in ${FILES_LIST[@]}
+do
+  echo $i $(basename "$file") $(stat --printf="%s" $file);
+  ((i++))
+done
+
+read -p 'Which File? ' selectedLog
+
+SELECTED_FILE=${FILES_LIST[$selectedLog]};
+
+echo Using $SELECTED_FILE;
+
+# NOTE: This uses ES6's string template literal (the grave quotation mark)
+echo "window.rawLog = \`" > $TOOLS_DIR/rawLog.js
+cat $SELECTED_FILE >> $TOOLS_DIR/rawLog.js
+echo "\`;" >> $TOOLS_DIR/rawLog.js
+
+echo "Done. rawLog.js size: " $(stat --printf="%s" $TOOLS_DIR/rawLog.js)
